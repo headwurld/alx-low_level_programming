22377c81433db075782f28d9b80409be860ef3a6
headwurld <ihedpros@gmail.com>
2023-08-12 10:19:46 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+ /**
+  * main - Entry point
+  *  Description: Prints numbers in base 16, from 0 to 9 and then a to f
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+
+{
+
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	}
+
+	for (low = 'a'; low <= 'f'; low++)
+	{
+	putchar(low);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
